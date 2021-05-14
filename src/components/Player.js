74e3fa01f0c2a020fd90d9a55812816634071b92
Tamara Abualhsan <tabualhsan@gmail.com>
2021-05-14 12:15:36 -0700

    need to fix a bug on line 61 in player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight, faPause, } from "@fortawesome/free-solid-svg-icons";
-import {playAudio} from '../util'
+
 const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, songInfo, songs, setCurrentSong, setSongs}) => {
 
     useEffect(() => { const newSongs = songs.map((song) => {
@@ -32,6 +32,9 @@ const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, s
         }
 
     };
+    const trackAnim = {
+        transform :`translateX(${songInfo.animationPercentage}%)`
+    };
    
     const getTime = (time) => {
         return(
@@ -42,34 +45,27 @@ const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, s
         audioRef.current.currentTime = e.target.value
        setSongInfo({...songInfo, currentTime: e.target.value});
     };
-   const skipTrackHandler = (direction) => {
+   const skipTrackHandler = async (direction) => {
        let currentIndex = songs.findIndex((song) => song.id === currentSong.id );    
         if(direction === 'skip-forward'){
-            setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+        await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+        if(isPlaying) audioRef.current.play();
+            return;
         }
-        if(direction === "skip-back"){
-            if((currentIndex - 1 % songs.length === -1)){
-                setCurrentSong(songs[songs.length -1]);
-                playAudio(isPlaying, audioRef);
-                return;
-            }
-                setCurrentSong(songs[(currentIndex - 1 ) % songs.length]);
+        await setCurrentSong(songs[(currentIndex - 1 ) % songs.length]);
         }
-        playAudio(isPlaying, audioRef);
-
+       if(isPlaying) audioRef.current.play();
    };
 
-   const trackAnim = {
-       transform :`translateX(${songInfo.animationPercentage}%)`
-   }
-    return(
+  
+    return (
         <div className="player">
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
+                {/* color gradient for time bar for each song */}
                 <div style= {{background: `linear-gradient(to right, ${currentSong.color[0]},${currentSong.color[1]})`,
                 }}
                 className="track">
-            
                 <input 
                 min= {0} 
                 max={songInfo.duration || 0 } 
@@ -102,13 +98,12 @@ const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, s
                 size ="2x" 
                 icon={faAngleRight} 
                 onClick={() => skipTrackHandler('skip-forward')}/>
+            
             </div>
-           
-           
         </div>
        
     );
-};
+
 
 
 export default Player;
